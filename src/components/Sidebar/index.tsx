10dcb086b9e883299878dc0237976ca0c9dc34d5
Pedro Conrado <conradohpedro@gmail.com>
2022-08-25 19:05:37 -0300

    create Sidebar component

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,22 @@
+import { TabButtonn } from "../TabButton";
+import { Container } from "./styles";
+
+export function Sidebar() {
+  return (
+    <Container>
+      <header>
+        <h2>.done</h2>
+      </header>
+
+      <main>
+        <TabButtonn />
+       
+      </main>
+
+      <footer>
+        
+      </footer>
+
+    </Container>
+  );
+}
---
+++ src/components/Sidebar/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.aside`
+    width: 20vw;
+    height: 100vh;
+    border-right: 1px solid var(--grey);
+`
