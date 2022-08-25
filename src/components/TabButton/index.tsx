0f7960f0180ef5d4fa44fbbbe8c24c1a4b095af0
Pedro Conrado <conradohpedro@gmail.com>
2022-08-25 19:06:03 -0300

    create TabButton component

---
+++ src/components/TabButton/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles";
+import { House } from "phosphor-react";
+
+export function TabButtonn() {
+  return (
+    <Container>
+      <House color="#999999" size={24} />
+
+      <span>Overview</span>
+      
+    </Container>
+  );
+}
---
+++ src/components/TabButton/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+    // Button styles
+    width: 10rem;
+    height: 2rem;
+    display: flex;
+    gap: 1rem;
+    justify-content: center;
+    align-items: center;
+
+    // Text styles
+    color: #999999;
+
+`
