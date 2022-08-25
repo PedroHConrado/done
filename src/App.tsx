fd9bd10535f2f71a3447622e587b1b6260ca84bd
Pedro Conrado <conradohpedro@gmail.com>
2022-08-25 19:04:38 -0300

    create project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Sidebar } from "./components/Sidebar";
+import { GLobalStyles } from "./styles/GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GLobalStyles />
+      <Sidebar />
+    </>
+   
+  );
+}
+
+export default App;
