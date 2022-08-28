32dd3126b14353573c52cec01366e10e2f675543
Pedro Conrado <conradohpedro@gmail.com>
2022-08-27 21:35:38 -0300

    style TabButton component

--- src/components/TabButton/index.tsx
+++ src/components/TabButton/index.tsx
@@ -1,13 +1,29 @@
 import { Container } from "./styles";
-import { House } from "phosphor-react";
+import { Calendar, ChartBar, ChatDots, FolderOpen, Gear, House, SignOut } from "phosphor-react";
 
-export function TabButtonn() {
+interface TabButtonProps {
+  text: string;
+}
+
+export function TabButton({ text }: TabButtonProps) {
+  function renderIcon() {
+    switch(text) {
+      case 'Overview': return <House color="#999999" size={24}/>;
+      case 'Stats': return <ChartBar color="#999999" size={24} />;
+      case 'Projects': return <FolderOpen color="#999999" size={24} />;
+      case 'Chat': return <ChatDots color="#999999" size={24} />;
+      case 'Calendar': return <Calendar color="#999999" size={24} />;
+      case 'Settings': return <Gear color="#999999" size={24} />;
+      case 'Log out': return <SignOut color="#999999" size={24} />;
+    }
+  }
+  
   return (
     <Container>
-      <House color="#999999" size={24} />
-
-      <span>Overview</span>
-      
+      <button>
+        {renderIcon()}
+        <span>{text}</span>
+      </button>
     </Container>
   );
 }
--- src/components/TabButton/styles.ts
+++ src/components/TabButton/styles.ts
@@ -1,15 +1,33 @@
 import styled from "styled-components";
 
-export const Container = styled.header`
+export const Container = styled.div`
     // Button styles
-    width: 10rem;
-    height: 2rem;
-    display: flex;
-    gap: 1rem;
-    justify-content: center;
-    align-items: center;
+    
+    button {
+        width: 100%;
+        height: 2rem;
+        display: flex;
+        justify-content: start;
+        align-items: center;
+        gap: 1rem;
+
+        border: none;
+
+        color: #999999;
+        background: transparent;
+        font-size: 1.2em;
+
+        cursor: pointer;
+        
+    }
+
+    button:focus {
+        border-right: 3px solid var(--green);
+        color: black;
+        font-weight: 500;
+    }
 
     // Text styles
-    color: #999999;
+    
 
 `
