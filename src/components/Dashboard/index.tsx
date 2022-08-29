e5776c6cea6e52e5a3b281428206547a2cc06380
Pedro Conrado <conradohpedro@gmail.com>
2022-08-29 18:45:16 -0300

    create Dashboard component

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import { Bell, CaretDown, Question } from "phosphor-react";
+import { Container } from "./styles";
+
+export function Dashboard() {
+  
+  return (
+    <Container>
+      <h1>Dashboard</h1>
+    </Container>
+  );
+}
