51c05383aac1b2cc7077306e320cbc5a9cc5809b
Pedro Conrado <conradohpedro@gmail.com>
2022-10-27 16:52:17 -0300

    create Task component

---
+++ src/components/Task/index.tsx
@@ -0,0 +1,17 @@
+import { Container } from "./styles";
+
+export function Task() {
+  return (
+    <Container>
+      <div className="type">
+        <span>Design</span>
+      </div>
+
+      <p className="description">
+        We need 2 different concepts for a software page in our program.
+
+        I've attached 2 concepts that will give you an idea to reproduce but with a new look and fell. We'd like to keep the colors similar but yo can add different colors.
+      </p>
+    </Container>
+  );
+}
